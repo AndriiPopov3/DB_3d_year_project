3b80fb18fe6cc59b88e427642413c3c5b2c7b233
AndriiPopov3 <66309913+AndriiPopov3@users.noreply.github.com>
2021-05-08 23:45:42 +0300

    Update query_task_12.sql

--- queries/query_task_12.sql
+++ queries/query_task_12.sql
@@ -29,3 +29,13 @@ SELECT j.station_id, j.call_number FROM (SELECT station_id, COUNT(*) AS "call_nu
 FROM constituent_stations_intercity_calls
 GROUP BY station_id 
 ORDER BY call_number DESC) j WHERE j.call_number < 3;
+
+SELECT COUNT(jp.station_id) FROM (SELECT j.station_id, j.call_number FROM (SELECT station_id, COUNT(*) AS "call_number"
+FROM departmental_stations_intercity_calls
+GROUP BY station_id 
+ORDER BY call_number DESC) j WHERE j.call_number < 3) jp;
+
+SELECT COUNT(jp.station_id) FROM (SELECT j.station_id, j.call_number FROM (SELECT station_id, COUNT(*) AS "call_number"
+FROM constituent_stations_intercity_calls
+GROUP BY station_id 
+ORDER BY call_number DESC) j WHERE j.call_number < 3) jp;
