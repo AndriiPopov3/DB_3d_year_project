18707ee26939d3197e9223f8522ad99f9d34cba1
AndriiPopov3 <66309913+AndriiPopov3@users.noreply.github.com>
2021-05-19 02:42:44 +0300

    minor adjustments

--- queries/query_task_3.sql
+++ queries/query_task_3.sql
@@ -1,9 +1,18 @@
 use course_work_db_1;
 
-SELECT * FROM subscribers WHERE station_id = 1 AND subscriber_id IN (SELECT subscriber_id FROM subscriber_fees WHERE subscriber_debt != 0 OR subscriber_intercity_debt != 0);
+SELECT * FROM subscribers 
+WHERE station_id = 1 
+AND subscriber_id IN (SELECT subscriber_id 
+					  FROM subscriber_fees 
+                      WHERE subscriber_debt != 0 
+                      OR subscriber_intercity_debt != 0);
 SELECT COUNT(subscriber_id) FROM subscribers WHERE station_id = 1 AND subscriber_id IN (SELECT subscriber_id FROM subscriber_fees WHERE subscriber_debt != 0 OR subscriber_intercity_debt != 0);
 
-SELECT * FROM subscribers WHERE subscriber_id IN (SELECT subscriber_id FROM subscriber_fees WHERE subscriber_debt != 0 OR subscriber_intercity_debt != 0);
+SELECT * FROM subscribers 
+WHERE subscriber_id IN (SELECT subscriber_id 
+						FROM subscriber_fees 
+                        WHERE subscriber_debt != 0 
+                        OR subscriber_intercity_debt != 0);
 SELECT COUNT(subscriber_id) FROM subscribers WHERE subscriber_id IN (SELECT subscriber_id FROM subscriber_fees WHERE subscriber_debt != 0 OR subscriber_intercity_debt != 0);
 
 SELECT * FROM subscribers WHERE subscriber_address_district = 'Lower West Prewun' AND subscriber_id IN (SELECT subscriber_id FROM subscriber_fees WHERE subscriber_debt != 0 OR subscriber_intercity_debt != 0);
